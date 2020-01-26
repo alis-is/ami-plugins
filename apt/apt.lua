2d4f9781ffc7d53fccc36475db68a5b30e8ece81
cryi <cryi@tutanota.com>
2020-01-26 21:16:31 +0100

    initial

---
+++ apt/apt.lua
@@ -0,0 +1,168 @@
+local util = require "eli.util"
+local values, generate_safe_functions = util.values, util.generate_safe_functions
+local eprocLoaded, eproc = pcall(require, "eli.proc.extra")
+local eenvLoaded, eenv = pcall(require, "eli.env.extra")
+local fs = require "eli.fs"
+local is_tty = require "is_tty".is_stdout_tty()
+
+require "eli.extensions.string"
+local _trace, _debug = require"eli.util".global_log_factory("plugin/apt", "trace", "debug")
+
+local function get_apt_binary()
+    local os = require "os"
+    _trace"Looking for apt binary"
+    if os.execute('apt -v 2>&1 >/dev/null') then
+        _debug"'apt' available."
+        return 'apt'
+    elseif os.execute('apt-get -v 2>&1 >/dev/null') then
+        _debug"'apt-get' available."
+        return 'apt-get';
+    else
+        _debug"No APT binary found."
+        return nil;
+    end
+end
+
+local function colorize_error_msg(msg)
+    if type(msg) ~= 'string' then return msg end
+    local _end = ''
+    if msg:sub(#msg,#msg) == '\n' then 
+       msg = msg:sub(1, #msg - 1)
+       _end = '\n'
+    end
+    return string.char(27) .. "[31m" .. msg .. string.char(27) .. "[0m" .. _end
+end
+
+local APT = get_apt_binary()
+assert(APT, "No supported 'apt' binary found")
+assert(eprocLoaded, "APT plugin requires posix proc extra api (eli.proc.extra)")
+assert(eenvLoaded, "APT plugin requires posix env extra api (eli.env.extra)")
+
+local function _execute(cmd, args, options)
+    _trace"Creating pipes"
+    local rd, proc_wr = fs.pipe()
+    local rderr, proc_werr = fs.pipe()
+    rd:set_nonblocking(true); rderr:set_nonblocking(true)
+    _trace"pipes created"
+
+    if type(options) ~= 'table' then 
+        options = {}
+    end
+    _trace{msg = "Spawning " .. APT, args = args, env = options.env, cmd = cmd}
+    
+    local proc, err = eproc.spawn{stdout = proc_wr, stderr = proc_werr, command = APT, args = args, env = options.env}
+    proc_wr:close(); proc_werr:close()
+
+    if not proc then
+        _debug{msg = "Failed to start " .. APT, error = err }
+        return false, -1, err, err
+    end
+    
+    local stdout, stderr = '', ''
+
+    if type(options.stdout_cb) ~= 'function' and type(options.stderr_cb) ~= 'function' then
+        proc:wait()
+        stdout = rd:read "a"
+        stderr = rderr:read "a"
+    else
+        while not proc:exited() do
+
+            local noOutput = false
+            if type(options.stdout_cb) == 'function' then 
+                repeat 
+                    local tmp = rd:read("L")
+                    stdout = stdout .. (tmp or '')
+                    options.stdout_cb(tmp)
+                    noOutput = tmp == ''
+                until (noOutput) 
+            end
+
+            if type(options.stderr_cb) == 'function' then 
+                repeat 
+                    local tmp = rderr:read("L")
+                    stderr = stderr .. (tmp or '')
+                    noOutput = tmp == ''
+                    if options.colorful or (options.colorful == nil and is_tty) then 
+                        tmp = colorize_error_msg(tmp)
+                    end
+                    options.stderr_cb(tmp)
+                until (noOutput) 
+            end
+            eproc.sleep(1)
+        end
+
+        proc:wait()
+        stdout = stdout .. rd:read('a')
+        stderr = stderr .. rderr:read('a')
+    end
+    local exitCode = proc:exitcode()
+    _trace{msg = APT .. " exited", exitcode = exitCode, stdout = stdout, stderr = stderr}
+    return exitCode == 0, exitCode, stdout, stderr
+end
+
+local function install(dependencies, options)
+    _debug{msg = "Installing dependencies...", dependencies = dependencies}
+    if type(dependencies) == 'table' then
+        dependencies = values(dependencies)
+    elseif type(dependencies) == 'string' then
+        dependencies = dependencies:split("%S+")
+    end
+    
+    for i, dependency in ipairs(dependencies) do
+        local success, exitcode, stdout, stderr = _execute(APT, {'install', '-y', dependency}, options)
+        if not success then
+            _debug{msg = "Failed to install dependency - " .. dependency .. ". APT stopped...", dependency = dependency, dependencies = dependencies}
+            return false, exitcode, stdout, stderr, dependency
+        end
+    end
+    _debug{msg = "Dependencies successfuly installed.", dependencies = dependencies}
+    return true
+end
+
+local function install_non_interactive(dependencies, options)
+    if type(options) ~= 'table' then options = {} end
+    local env = options.env or eenv.environment()
+    env.DEBIAN_FRONTEND = 'noninteractive'
+    options.env = env
+    return install(dependencies, options)
+end
+
+local function upgrade(options)
+    local success, exitcode, stdout, stderr = _execute(APT, {'upgrade', '-y'}, options)
+    return success, exitcode, stdout, stderr
+end
+
+local function upgrade_non_interactive(options)
+    if type(options) ~= 'table' then options = {} end
+    local env = options.env or eenv.environment()
+    env.DEBIAN_FRONTEND = 'noninteractive'
+    options.env = env
+    return upgrade(dependencies, options)
+end
+
+local function update(options)
+    local success, exitcode, stdout, stderr = _execute(APT, {'update'}, options)
+    return success, exitcode, stdout, stderr
+end
+
+local function autoremove(options)
+    local success, exitcode, stdout, stderr = _execute(APT, {'autoremove'}, options)
+    return success, exitcode, stdout, stderr
+end
+
+local function clean(options)
+    local success, exitcode, stdout, stderr = _execute(APT, {'clean'}, options)
+    return success, exitcode, stdout, stderr
+end
+
+return generate_safe_functions({
+    upgrade = upgrade,
+    clean = clean,
+    install = install,
+    update = update,
+    autoremove = autoremove, 
+    get_apt_binary = get_apt_binary,
+    install_non_interactive = install_non_interactive,
+    upgrade_non_interactive = upgrade_non_interactive
+})
+
---
+++ platform/platform.lua
@@ -0,0 +1,87 @@
+
+local _trace, _debug = require"eli.util".global_log_factory("plugin/platform", "trace", "debug")
+
+local function _execute(cmd) 
+    local _f = io.popen(cmd)
+    local _output = _f:read"a*"
+    local _success, _exit, _code = _f:close()
+    return _success, _exit, _code, _output
+end
+
+local function _get_platform() 
+    local _delim = package.config:sub(1,1);
+    if _delim == '\\' then 
+        -- windows
+        _debug{ msg="Assuming windows platform." }
+        local _success, _exit, _code, _output = _execute('systeminfo.exe');
+        _trace{ msg=_output, type="stdout", external_source="systeminfo.exe", exitcode=_exit }
+        if not _success then 
+            _debug{ msg="Failed to get windows platform details." }
+            return false
+        else 
+            local _details = {
+                OS = "win",
+                OS_NAME = _output:match("OS Name:%s*([^\n]*)"),
+                OS_VERSION = _output:match("OS Version::%s*([^\n]*)"),
+                SYSTEM_TYPE = _output:match("System Type:%s*([^\n]*)")
+            }
+            _debug{ msg="Succefully got platform details.", details = _details }
+            return true, _details
+        end
+    else 
+        -- unix
+        _debug{ msg="Assuming windows platform." }
+        local _success, _exit, _code, _output = _execute('lsb_release -a 2>/dev/null')
+        _trace{ msg=_output, type="stdout", external_source="lsb_release -a", exitcode=_exit }
+        if not _success then 
+            _debug{ msg="Failed to get unix platform details." }
+            return false 
+        end
+
+        local _DISTRO = _output:match("Distributor ID:%s*(.*)") or ''
+        local _DISTRO_VERSION = _output:match("Release:%s*(.*)") or ''
+
+        _success, _exit, _code, _output = _execute('uname -s')
+        _trace{ msg=_output, type="stdout", external_source="uname -s", exitcode=_exit }
+        if not _success then 
+            _debug{ msg="Failed to get unix platform details." }
+            return false 
+        end
+        local _KERNEL = _output:match("([^\n]*)") or ''
+
+        _success, _exit, _code, _output = _execute('uname -r')
+        _trace{ msg=_output, type="stdout", external_source="uname -r", exitcode=_exit }
+        if not _success then 
+            _debug{ msg="Failed to get unix platform details." }
+            return false 
+        end
+        local _KERNEL_VERSION = _success and _output:match("([^\n]*)") or ''
+
+        _success, _exit, _code, _output = _execute('uname -m')
+        _trace{ msg=_output, type="stdout", external_source="uname -m", exitcode=_exit }
+        local _SYSTEM_TYPE = _success and _output:match("([^\n]*)") or ''
+
+        _success, _exit, _code, _output = _execute('uname -i')
+        _trace{ msg=_output, type="stdout", external_source="uname -i", exitcode=_exit }
+        local _PLATFORM_TYPE = _success and _output:match("([^\n]*)") or ''
+
+        _success, _exit, _code, _output = _execute('uname -p')
+        _trace{ msg=_output, type="stdout", external_source="uname -p", exitcode=_exit }
+        local _PROCESSOR_TYPE = _success and _output:match("([^\n]*)") or ''
+
+        local _details = {
+            OS = "unix",
+            DISTRO = _DISTRO,
+            DISTRO_VERSION = _DISTRO_VERSION,
+            KERNEL = _KERNEL,
+            KERNEL_VERSION = _KERNEL_VERSION,
+            SYSTEM_TYPE = _SYSTEM_TYPE,
+            PLATFORM_TYPE = _PLATFORM_TYPE,
+            PROCESSOR_TYPE = _PROCESSOR_TYPE
+        }
+        _debug{ msg="Succefully got platform details.", details = _details }
+        return true, _details
+    end
+end
+
+return { get_platform = _get_platform }
